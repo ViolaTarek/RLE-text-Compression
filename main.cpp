028883b43c6b3eceef3d752a160cbfb0224ab31f
Viola Tarek <veolazaher@gmail.com>
2018-02-25 18:20:19 -0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -7,27 +7,42 @@
 using namespace std;
 string to_string(int n);
 string Compress(string input);
+string decompress(string Output);
 int main()
 {
-
     ifstream inFile;
     ofstream outFile;
+    ifstream inFileComp;
+    ofstream outFilede;
     string str;
+    string str2;
+    outFilede.open("Decompressed.txt");
     outFile.open("PhoneListCompressed.txt");
     inFile.open("PhoneList.txt");
+    inFileComp.open("comp.txt");
     if (!inFile.is_open())
     {
         exit(EXIT_FAILURE) ;
 
     }
+    //compress from infile to outfile
 
     while( getline(inFile,str))
     {
-
         cout<<Compress(str)<<endl;
         outFile<<Compress(str);
     }
 
+    //decompress from infilecomp to outfilede
+
+
+    while(getline(inFileComp,str2))
+    {
+        cout<< decompress(str2);
+        outFilede<<decompress(str2);
+
+
+    }
     inFile.close();
 
     return 0;
@@ -43,10 +58,18 @@ string Compress(string input)
         if(input[i]!=' ')
         {
             while (input[i] == input[i + 1])
-                Count++, i++;
+                Count++,i++;
             if(Count>2)
+            {
                 encoding += '@'+to_string(Count) + input[i];
+            }
 
+            else if(Count==2)
+            {
+
+                encoding += input[i];
+                encoding += input[i];
+            }
             else
                 encoding += input[i];
         }
@@ -54,7 +77,33 @@ string Compress(string input)
     return encoding;
 
 }
+string decompress(string Output)
+{
+    int z;
+    string decoding="";
+    for (int i=0 ; i<Output.length() ; i++)
+    {
+        if ( Output[i] == '@')
+        {
+            z =Output[i+1] - '0' ;
+            while (z>0)
+            {
+                decoding += Output[i+2] ;
+                z--;
 
+            }
+            i+=2;
+        }
+        else
+        {
+            decoding += Output[i] ;
+        }
+
+    }
+    return decoding;
+
+
+}
 string to_string ( int n )
 {
     stringstream ss;
